a167287f3a30c30b4ac5e68ac2ce9ad269819381
Nicolas Lazarte <1853973@crosemont.qc.ca>
2022-11-18 16:39:16 -0500

    Ajout du controlleur patient

--- ServiceCliniqueRosemont/Controllers/PatientController.cs
+++ ServiceCliniqueRosemont/Controllers/PatientController.cs
@@ -10,9 +10,27 @@ namespace ServiceCliniqueRosemont.Controllers
     {
         [HttpGet]
         [Route("GetAll")]
-        public void GetAll()
+        public List<Patient> GetAll()
         {
+            var dao = new PatientDAO();
+            var lstMed = dao.AvoirTousLesPatients();
+            return lstMed;
+        }
 
+        [HttpPut]
+        [Route("Add")]
+        public void Add(string nom, string prenom, string password, string email, string ddn, string age, string sexe, string allergies)
+        {
+            var dao = new PatientDAO();
+            var lstPatient = dao.AjouterUnPatient(prenom, nom, password, email, ddn, age, sexe, allergies);
+        }
+
+        [HttpPut]
+        [Route("Remove")]
+        public void Remove(int id)
+        {
+            var dao = new PatientDAO();
+            var lstPatient = dao.SupprimerUnPatient(id);
         }
     }
 }
--- ServiceCliniqueRosemont/Source/PatientDAO.cs
+++ ServiceCliniqueRosemont/Source/PatientDAO.cs
@@ -1,8 +1,93 @@
-﻿namespace ServiceCliniqueRosemont.Source
+﻿using ExamBiblio.Source;
+using ServiceCliniqueRosemont.Model;
+using System.Data.SqlClient;
+
+namespace ServiceCliniqueRosemont.Source
 {
     public class PatientDAO
     {
+        const string SQL_SELECT_ALL = "SELECT * FROM PATIENTS;";
 
+        const string SQL_CREATE = "INSERT INTO `PATIENTS` (`nom`, `prenom`, `password`, `email`, `ddn`, `age`, `sexe`, `allergies`) VALUES (@nom, @prenom, @password, @email, @ddn, @age, @sexe, @allergies);";
+
+        const string SQL_DELETE = "DELETE FROM `PATIENTS` WHERE (id=?";
+
+        const string SQL_ORDER_BY = "SELECT * FROM PATIENTS" +
+            "ORDER BY id";
+
+        public List<Patient> AvoirTousLesPatients()
+        {
+            Connecteur.Connect();
+            var lstpatients = new List<Patient>();
+            var command = Connecteur.connection.CreateCommand();
+            command.CommandText = SQL_SELECT_ALL;
+            var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                var patient = new Patient
+                {
+                    Id = reader.GetInt32("id"),
+                    Nom = reader.GetString("nom"),
+                    Prenom = reader.GetString("prenom"),
+                    Password = reader.GetString("password"),
+                    Email = reader.GetString("email"),
+                    Ddn = reader.GetString("ddn"),
+                    Age = reader.GetString("age"),
+                    Sexe = reader.GetString("sexe"),
+                    Allergies = reader.GetString("allergies")
+                };
+                lstpatients.Add(patient);
+            }
+            Connecteur.Disconnect();
+            return lstpatients;
+        }
+
+        public bool AjouterUnPatient(string nom, string prenom, string password, string email, string ddn, string age, string sexe, string allergies)
+        {
+            Connecteur.Connect();
+            var command = Connecteur.connection.CreateCommand();
+            command.CommandText = SQL_CREATE;
+            command.Parameters.AddWithValue("@nom", nom);
+            command.Parameters.AddWithValue("@prenom", prenom);
+            command.Parameters.AddWithValue("@password", password);
+            command.Parameters.AddWithValue("@email", email);
+            command.Parameters.AddWithValue("@ddn", ddn);
+            command.Parameters.AddWithValue("@age", age);
+            command.Parameters.AddWithValue("@sexe", sexe);
+            command.Parameters.AddWithValue("@allergies", allergies);
+
+            var reader = command.ExecuteReader();
+            var isSucces = reader != null;
+            Connecteur.Disconnect();
+            return isSucces;
+        }
+
+        public bool SupprimerUnPatient(int id)
+        {
+            Connecteur.Connect();
+            var command = Connecteur.connection.CreateCommand();
+            command.CommandText = SQL_DELETE;
+            command.Parameters.Remove(id);
+
+            var reader = command.ExecuteReader();
+            var isSucces = reader != null;
+            Connecteur.Disconnect();
+            return isSucces;
+        }
+
+        public bool listerUnPatientParID(int id)
+        {
+            Connecteur.Connect();
+            var command = Connecteur.connection.CreateCommand();
+            command.CommandText = SQL_ORDER_BY;
+            //int v = command.Parameters.Add(new SqlParameter(id));
+
+            var reader = command.ExecuteReader();
+            var isSucces = reader != null;
+            Connecteur.Disconnect();
+            return isSucces;
+        }
 
 
     }
