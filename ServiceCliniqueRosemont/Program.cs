946b006aa3016b5395b651e9a02c1375e93aa346
nbrunet <nbrunet@dept-info.crosemont.quebec>
2022-11-21 22:16:52 -0500

    Correction d'un problem avec Cors pour angular

--- ServiceCliniqueRosemont/Program.cs
+++ ServiceCliniqueRosemont/Program.cs
@@ -16,6 +16,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(x => x
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+
 app.UseAuthorization();
 
 app.MapControllers();
